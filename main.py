836eee754e75eb65a050f0d2b3147646034a638a
dimasajipangestu <dimasajipangestu@gmail.com>
2020-11-11 15:00:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 def read_file(country_code):
     with open("./dataset/"+country_code+".json", "r") as read_file:
